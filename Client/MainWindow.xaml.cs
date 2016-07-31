97ab77da5a9e238a46d7c649e80a6bb0c7e65eb5
Ekaterina Mashkovceva <mashkovceva_e_a@bk.ru>
2016-07-31 21:09:35 +0500

    Добавление файлов

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        partial class ClientObject
+        {
+            public Socket socket = null;
+            public const int bufferSize = 2;
+            public byte[] buffer = new byte[bufferSize];
+        }
+
+        Socket client;
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+        byte[] outData = new byte[2];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Connect();
+            
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                //IPHostEntry ipHostInfo = Dns.Resolve("localhost");
+                //IPAddress ipAddress = ipHostInfo.AddressList[0];
+                //IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8888);
+
+                IPAddress ipAddress = IPAddress.Parse("192.168.1.13");
+                IPEndPoint endPoint = new IPEndPoint(ipAddress, 8888);
+
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                client.BeginConnect(endPoint, new AsyncCallback(ConnectCallback), client);
+                connectDone.WaitOne();
+
+                lbLog.Items.Add("WaitOne");
+
+                Receive(client);
+
+                lbLog.Items.Add("Receive(client)");
+            }
+            catch (Exception e)
+            {
+                lbLog.Items.Add(e.ToString());
+            }
+        }
+
+        private void Receive(Socket client)
+        {
+            try
+            {
+                ClientObject state = new ClientObject();
+                state.socket = client;
+                client.BeginReceive(state.buffer, 0, ClientObject.bufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                lbLog.Items.Add(e.ToString());
+            }
+        }
+        private static String response = String.Empty;
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                ClientObject state = (ClientObject)ar.AsyncState;
+                Socket client = state.socket;
+
+                // Read data from the remote device.
+                int bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 1)
+                {
+                    // There might be more data, so store the data received so far.
+                    //state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                    // Get the rest of the data.
+                    //lbLog.Items.Add("state.buffer " + state.buffer[0] + " " + state.buffer[1]);
+                    parsData(state.buffer);
+                    
+
+
+                }
+                client.BeginReceive(state.buffer, 0, ClientObject.bufferSize, 0,
+                        new AsyncCallback(ReceiveCallback), state);
+                //else
+                //{
+                //    // All the data has arrived; put it in response.
+                //    if (state.sb.Length > 1)
+                //    {
+                //        //lbLog.Items.Add(state.sb.ToString());
+                //        parsData(state.sb.ToString());
+                //    }
+                //    // Signal that all bytes have been received.
+                //}
+            }
+            catch (Exception e)
+            {
+                //lbLog.Items.Add(e.ToString());
+            }
+        }
+
+        private void parsData(byte[] buffer)
+        {
+            if (buffer[0] == 1)
+            {
+                btnStartStop.Dispatcher.Invoke(new Action(delegate ()
+                {
+                    btnStartStop.Content = "Stop";
+                })); 
+            }
+            else
+            {
+                btnStartStop.Dispatcher.Invoke(new Action(delegate ()
+                {
+                    btnStartStop.Content = "Start";
+                }));
+            }
+
+            lblTimer.Dispatcher.Invoke(new Action(delegate ()
+            {
+                lblTimer.Content = buffer[1].ToString();
+            }));
+        }
+
+        
+
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                connectDone.Set();
+                Socket clientSocket = (Socket)ar.AsyncState;
+                clientSocket.EndConnect(ar);
+                //lbLog.Items.Add("Socket connected to " + clientSocket.RemoteEndPoint.ToString());
+                
+            }
+            catch (Exception e)
+            {
+                //lbLog.Items.Add(e.ToString());
+            }
+        }
+
+        private void btnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            //byteData = new byte[2];
+            if (btnStartStop.Content.ToString() == "Start")
+            {
+                outData[0] = 1;
+                btnStartStop.Content = "Stop";
+            }
+            else
+            {
+                outData[0] = 0;
+                btnStartStop.Content = "Start";
+            }
+            //client.Send(byteData);
+            client.BeginSend(outData, 0, outData.Length, 0, new AsyncCallback(SendCallback), client);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket clientSocket = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.
+                int bytesSent = clientSocket.EndSend(ar);
+                //lbLog.Items.Add("Sent " + bytesSent + " bytes to server.");
+
+            }
+            catch (Exception e)
+            {
+                //lbLog.Items.Add(e.ToString());
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Server
+{
+    class Program
+    { 
+        public class Client
+        {
+            public Socket socket = null;
+            public const int bufferSize = 2;
+            public byte[] buffer = new byte[bufferSize];
+            public StringBuilder sb = new StringBuilder();
+        }
+        public class StateObject
+        {
+            public System.Timers.Timer timer;
+            public int timerValue;
+            public List<Socket> clients;
+        }
+
+        public static StateObject stateObject;
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public static void StartListening()
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.1.13");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8888);
+
+            
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Bind(localEndPoint);
+                server.Listen(100);
+
+                while (true)
+                {
+                    allDone.Reset();
+
+                    Console.WriteLine("Waiting for a connection...");
+                    server.BeginAccept(new AsyncCallback(AcceptCallback), server);
+
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket server = (Socket)ar.AsyncState;
+            Socket handler = server.EndAccept(ar);
+
+            if (!stateObject.clients.Contains(handler))
+            {
+                stateObject.clients.Add(handler);
+            }
+
+            Console.WriteLine("Socket connected to " + handler.RemoteEndPoint.ToString());
+            Client client = new Client();
+            client.socket = handler;
+            handler.BeginReceive(client.buffer, 0, Client.bufferSize, 0,new AsyncCallback(ReadCallback), client);
+        }
+
+        private static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+            Client client = (Client)ar.AsyncState;
+            Socket handler = client.socket;
+
+
+
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                parsData(handler, client.buffer);
+                Console.WriteLine(Encoding.ASCII.GetString(client.buffer, 0, bytesRead));
+
+                //client.sb.Append(Encoding.ASCII.GetString(client.buffer, 0, bytesRead));
+
+                //content = client.sb.ToString();
+                //if (content.IndexOf("<EOF>") > -1)
+                //{
+                //    parsData(handler, content);
+                //}
+                //else
+                //{
+                //    handler.BeginReceive(client.buffer, 0, Client.bufferSize, 0, new AsyncCallback(ReadCallback), client);
+                //}
+            }
+            handler.BeginReceive(client.buffer, 0, Client.bufferSize, 0, new AsyncCallback(ReadCallback), client);
+        }
+
+        private static void parsData(Socket handler, byte[] byteData)
+        {
+            //byte[] byteData = Encoding.ASCII.GetBytes(content);
+
+            
+
+            if (byteData[0] == 1)
+            {
+                stateObject.timer.Enabled = true;
+            }else
+            {
+                stateObject.timer.Enabled = false;
+                onTimerTick(stateObject.timer.Enabled);
+            }
+        }
+        private static byte[] byteData = new byte[2];
+        private static void onTimerTick(bool enabled)
+        {
+            byteData[0] = (byte)(enabled ? 1 : 0);
+            byteData[1] = (byte)stateObject.timerValue;
+
+            Console.WriteLine("byteData " + byteData[0] + " " + byteData[1]);
+
+            foreach (Socket socket in stateObject.clients)
+            {
+                socket.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), socket);
+            }
+        }
+
+        private static void timerTick(Object source, ElapsedEventArgs e)
+        {
+            if (stateObject.timerValue < 255)
+            {
+                stateObject.timerValue++;
+            }else
+            {
+                stateObject.timerValue = 0;
+            }
+            onTimerTick(stateObject.timer.Enabled);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client {1}", bytesSent, handler.RemoteEndPoint.ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            stateObject = new StateObject();
+
+            stateObject.clients = new List<Socket>();
+
+            stateObject.timer = new System.Timers.Timer(1000);
+            stateObject.timer.Elapsed += new ElapsedEventHandler(timerTick);
+            stateObject.timer.Enabled = false;
+
+            stateObject.timerValue = 0;
+
+            StartListening();
+        }
+    }
+}
